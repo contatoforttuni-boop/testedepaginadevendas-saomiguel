bd4fe1ad656d7e87bf2114c3e535f413d9c8331c
contatoforttuni-boop <contato.forttuni@gmail.com>
2026-07-28 18:18:37 -0300

    Configurar temporada para contas

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.LCR_CONFIG={supabaseUrl:'https://cyyvguwiocbjyivixkrf.supabase.co',supabaseKey:'sb_publishable_YxZTbtvtLyQoeu0FxL6ZBg_gklNH8kv',quizSlug:'catolico-raiz-dia-1',checkoutUrl:'#checkout-pendente',whatsappUrl:'#whatsapp-pendente'};
+window.LCR_CONFIG={supabaseUrl:'https://cyyvguwiocbjyivixkrf.supabase.co',supabaseKey:'sb_publishable_YxZTbtvtLyQoeu0FxL6ZBg_gklNH8kv',quizSlug:'catolico-raiz-dia-1',seasonSlug:'temporada-fundadores',checkoutUrl:'#checkout-pendente',whatsappUrl:'#whatsapp-pendente'};
