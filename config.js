5948a546251e6df5760bfea97795f7289ee6ad92
contatoforttuni-boop <contato.forttuni@gmail.com>
2026-07-28 17:27:19 -0300

    Adicionar configuração do aplicativo

---
+++ config.js
@@ -0,0 +1,8 @@
+window.LCR_CONFIG = {
+  supabaseUrl: 'https://cyyvguwiocbjyivixkrf.supabase.co',
+  supabaseKey: 'sb_publishable_YxZTbtvtLyQoeu0FxL6ZBg_gklNH8kv',
+  quizSlug: 'catolico-raiz-dia-1',
+  checkoutUrl: '#checkout-pendente',
+  whatsappUrl: '#whatsapp-pendente',
+  adminPasswordHash: 'e4022c600f3d1268085d3ba98e5fcd91440448a38728ee03162551b4519490b6'
+};
